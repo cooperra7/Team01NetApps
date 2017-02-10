e40257738ccc70e3d844cd72d52c701a83a96ce3
Riley Cooper <cooperra@vt.edu>
2017-02-10 17:07:08 +0000

    Created script to go from incoming tweet string to a message

---
+++ cooperra/tweettomessage.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import hashlib
+import sys
+import re
+
+if len(sys.argv) != 2:
+    print ("Usage: {} <message>".format(sys.argv[0]))
+    sys.exit(1)
+
+def getmd5 (message):
+    m = hashlib.md5()
+    m.update(message.encode('utf-8'))
+    return m.hexdigest()
+
+def makemessage (message):
+    return (message, getmd5(message))
+
+if re.match('.*?@tomswift ECE4564-Team01.*?', sys.argv[1]):
+    fields = sys.argv[1].split('_')
+    if re.match('^[12]?[0-9]?[0-9]\.[12]?[0-9]?[0-9]\.[12]?[0-9]?[0-9]\.[12]?[0-9]?[0-9]:[1-6]?[0-9]?[0-9]?[0-9]?[0-9]$', fields[1]):
+        q = makemessage(fields[2])
+        print ("{}".format(q))
