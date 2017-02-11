d72980e0c6c7d433a2d9a07985907d4940bbb023
Riley Cooper <cooperra@vt.edu>
2017-02-11 18:41:41 +0000

    Added pickling to tweettomessage.py

--- cooperra/tweettomessage.py
+++ cooperra/tweettomessage.py
@@ -3,6 +3,7 @@
 import hashlib
 import sys
 import re
+import pickle
 
 if len(sys.argv) != 2:
     print ("Usage: {} <message>".format(sys.argv[0]))
@@ -21,3 +22,7 @@ if re.match('.*?@tomswift ECE4564-Team01.*?', sys.argv[1]):
     if re.match('^[12]?[0-9]?[0-9]\.[12]?[0-9]?[0-9]\.[12]?[0-9]?[0-9]\.[12]?[0-9]?[0-9]:[1-6]?[0-9]?[0-9]?[0-9]?[0-9]$', fields[1]):
         q = makemessage(fields[2])
         print ("{}".format(q))
+        s = pickle.dumps(q)
+        print("{}".format(s))
+        t = pickle.loads(s)
+        print ("{}".format(t))
