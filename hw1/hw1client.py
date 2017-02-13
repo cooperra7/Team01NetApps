0caf60ddf5936a4821bbf5a62460079258e12da7
skharimah <skharimah@users.noreply.github.com>
2017-02-12 21:28:06 -0500

    Append the Twitter streaming API part to hw1client.py

--- hw1/hw1client.py
+++ hw1/hw1client.py
@@ -5,11 +5,67 @@ import sys
 import re
 import pickle
 import socket
+import tweepy
+import twitteraccess
+import json
 
 host = 'localhost'
 port = 5000
 backlog = 5
 side = 1024
+
+"""
+Create class MyStreamListener inheriting from StreamListener to listen to stream and saves the resulting tweet to a text file.
+
+"""
+
+
+class MyStreamListener(tweepy.StreamListener):
+
+    def on_status(self, status):
+        print(status.text)
+
+    def on_data(self, raw_data):
+        data_in_json = json.loads(raw_data)
+        file = open('twitter_data.txt', 'w+')
+        file.write(data_in_json['text'])
+        file.close()
+        return False
+
+"""
+Read the "twitter_data.txt" where the last tweet is saved and return it as a string.
+
+:param: None
+:return: String of the latest tweet.
+
+"""
+
+
+def get_question_tweet_from_file():
+    file = open('twitter_data.txt', 'r')
+    twitter_data = file.read()
+    file.close()
+    return twitter_data
+
+"""
+Twitter Streaming API
+"""
+
+api = twitteraccess.get_twitter_api_handle()
+myStreamListener = MyStreamListener()
+myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
+
+result = myStream.filter(track=['@netapp_team01'])
+
+question_tweet = get_question_tweet_from_file()         # question_tweet contains the retrieved tweet
+print("question tweet is this : " + question_tweet)     # prints retrieved tweet
+
+msg = "here's a question : " + question_tweet
+# twitteraccess.send_tweet_to_timeline(api_handle=api, new_status_message=msg) # use this to tweet new message
+
+"""
+End of Twitter Streaming API
+"""
 
 if len(sys.argv) != 2:
     print ("Usage: {} <message>".format(sys.argv[0]))
