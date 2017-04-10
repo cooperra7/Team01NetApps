a39acc40f24c1c4f2d1279574faf860d6410d13c
Riley Cooper <cooperra@vt.edu>
2017-04-10 16:58:48 +0000

    Blockids

--- hw4/coapserver.py
+++ hw4/coapserver.py
@@ -14,6 +14,15 @@ mc = minecraft.Minecraft.create()
 # Global Variable for determining if row has been completed 
 row_complete = 0
 
+def mapblock(int blockid):
+    if blockid == 0:
+        return block.WOOD.id
+    elif blockid == 1:
+        return block.DIAMOND_BLOCK.id
+    elif blockid == 2:
+        return block.STONE.id
+    else
+        return block.DIRT.id
         
 class LocationResource (resource.Resource):
     
@@ -61,112 +70,6 @@ class LocationResource (resource.Resource):
             print('Content: {}'.format(self.content))
             payload = pickle.dumps ('Wall is done')
             return aiocoap.Message (payload=payload)
-
-def main ():
-
-    root = resource.Site()
-    root.add_resource (('location',), LocationResource())
-
-    asyncio.Task (aiocoap.Context.create_server_context(root))
-    asyncio.get_event_loop().run_forever()
-
-if __name__ == "__main__":
-    main()#!/usr/local/bin/python3
-
-import aiocoap
-import asyncio
-import aiocoap.resource as resource
-import pickle
-import sys
-import mcpi.minecraft as minecraft
-import mcpi.block as block
-
-#Global Variable for Minecraft Connection
-mc = minecraft.Minecraft.create()
-        
-class LocationResource (resource.Resource):
-
-    token_id = 0
-
-    def __init__(self):
-        super(LocationResource, self).__init__()
-        self.content = (0,0,0,0)
-
-
-    async def render_get (self, request):
-        temp = tuple(mc.player.getPos())
-        self.content = (temp[0], temp[1], temp[2], self.token_id)
-        g = pickle.dumps (self.content)
-        return aiocoap.Message(payload=g)
-
-    async def render_post (self, request):
-        p = pickle.loads (request.payload)
-        print ('POST: {}'.format (p))
-
-        if self.token_id > 2:
-            self.token_id = 0
-        else:
-            self.token_id += 1
-
-        self.content = (self.content[0],self.content[1],1+self.content[2],self.token_id)
-        mc.setBlock(self.content[0],self.content[1], self.content[2], block.DIRT.id)
-        mc.postToChat(self.content[0])
-        print ('Content: {}'.format(self.content))
-        payload = pickle.dumps ('POST request received to place block at {}'.format (p))
-        return aiocoap.Message (payload=payload)
-
-def main ():
-
-    root = resource.Site()
-    root.add_resource (('location',), LocationResource())
-
-    asyncio.Task (aiocoap.Context.create_server_context(root))
-    asyncio.get_event_loop().run_forever()
-
-if __name__ == "__main__":
-    main()#!/usr/local/bin/python3
-
-import aiocoap
-import asyncio
-import aiocoap.resource as resource
-import pickle
-import sys
-import mcpi.minecraft as minecraft
-import mcpi.block as block
-
-#Global Variable for Minecraft Connection
-mc = minecraft.Minecraft.create()
-        
-class LocationResource (resource.Resource):
-
-    token_id = 0
-
-    def __init__(self):
-        super(LocationResource, self).__init__()
-        self.content = (0,0,0,0)
-
-
-    async def render_get (self, request):
-        self.content = (tuple(mc.player.getPos()), self.token_id)
-        g = pickle.dumps (self.content)
-        return aiocoap.Message(payload=g)
-
-    async def render_post (self, request):
-        p = pickle.loads (request.payload)
-        print ('POST: {}'.format (p))
-
-        if self.token_id > 2:
-            self.token_id = 0
-        else:
-            self.token_id += 1
-
-        
-        mc.setBlock(self.content[0], self.content[1], 1+self.content[2], block.DIRT.id)
-        mc.postToChat('Block placed at ' + ' '.join(map(str, (self.content))))
-        self.content = (self.content[0], self.content[1], 1+self.content[2], self.token_id)
-        print ('Content: {}'.format(self.content))
-        payload = pickle.dumps ('POST request received to place block at {}'.format (p))
-        return aiocoap.Message (payload=payload)
 
 def main ():
 
