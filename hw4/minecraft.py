6b62e40897e111f3d7cfda7fd70355f697f74de5
Zachary Yee <zjwy99@vt.edu>
2017-04-09 20:25:56 -0400

    Create minecraft.py

---
+++ hw4/minecraft.py
@@ -0,0 +1,12 @@
+import time
+import sys
+import mcpi.minecraft as minecraft
+import mcpi.block as block
+
+if (len(sys.argv) != 2):
+    print ('usage: minecraft <ipaddress>')
+    sys.exit(2)
+
+host = sys.argv[1]
+mc = minecraft.Minecraft.create(host)
+mc.postToChat('Hello world')
