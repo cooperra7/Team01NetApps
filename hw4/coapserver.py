c7186fc4b6450aca00c5f95e03c693fadce3ec79
Zachary Yee <zjwy99@vt.edu>
2017-04-09 23:13:50 -0400

    post now sets block at position

--- hw4/coapserver.py
+++ hw4/coapserver.py
@@ -34,8 +34,9 @@ class LocationResource (resource.Resource):
         else:
             self.token_id += 1
 
-        self.content = (0,0,1+self.content[2],self.token_id)
-        mc.setBlock(self.content ,block.DIRT.id)
+        self.content = (self.content[0],self.content[1],1+self.content[2])
+        mc.setBlock(self.content[0], self.content[1], self.content[2], block.DIRT.id)
+        mc.postToChat('Block placed at ' + ' '.join(map(str, (self.content))))
         print ('Content: {}'.format(self.content))
         payload = pickle.dumps ('POST request received to place block at {}'.format (p))
         return aiocoap.Message (payload=payload)
