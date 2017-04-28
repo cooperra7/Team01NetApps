042c8926c5112ee5d47d2868a42839fc39e7930e
Riley Cooper <cooperra@vt.edu>
2017-04-28 01:11:58 +0000

    Client and server for rpc message brokering

--- final/accounts.py
+++ final/accounts.py
@@ -1,11 +1,49 @@
-import pymongo
+from pymongo import MongoClient
 import pika
 import sys
 import json
 import getopt
+import pickle
+import json
 
-# Need name, payment method, info for payment method, token, amount paid/items bought
+class Accounts(object):
+    def register(self, ch, method, properties, body):
+        print('Register')
+        ch.basic_publish(exchange='', routing_key=properties.reply_to,
+                                properties=pika.BasicProperties(correlation_id = properties.correlation_id),
+                                body = pickle.dumps('Registered'))
 
-# { "name" : string, "token" : int, "payment" : { "type" : string, "value" : int }, "amount" : int }
+    def pay(self, ch, method, properties, body):
+        print ('Pay')
+
+    def callback (self, ch, method, properties, body):
+        mess = pickle.loads (body)
+        obj = json.loads(mess)
+        if (obj['type'] == "register"):
+            self.register(ch, method, properties, body)
+        elif (obj['type'] == "pay"):
+            self.pay(ch, method, properties, body)
+        else:
+            print ('Not Valid')
+
+    def __init__(self):
+        self.client = MongoClient ('localhost', 27017)
+        self.db = self.client.accounts
+
+        vhost = '/'
+        mbservip = 'localhost'
+        creds = pika.PlainCredentials ('guest', 'guest')
+        params = pika.ConnectionParameters (virtual_host = vhost, credentials = creds, host=mbservip)
+        self.connection = pika.BlockingConnection (params)
+        self.channel = self.connection.channel()
+#        self.channel.exchange_declare (exchange = 'smartgroceries', type='direct')
+        result = self.channel.queue_declare (queue='rpc_queue', exclusive=True)
+        self.qname = result.method.queue
+
+#        self.channel.queue_bind (exchange='smartgroceries', queue=qname, routing_keys='accounts')
+
+        self.channel.basic_consume (self.callback, queue=self.qname, no_ack=True)
+        self.channel.start_consuming()
 
 
+q = Accounts()
---
+++ final/clientaccounts.py
@@ -0,0 +1,44 @@
+import pika
+import sys
+import json
+import pickle
+import json
+import uuid
+
+class Client (object):
+
+    def __init__(self):
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+
+        self.channel = self.connection.channel()
+
+        result = self.channel.queue_declare(exclusive=True)
+        self.callback_queue = result.method.queue
+
+        self.channel.basic_consume(self.on_response, no_ack=True,
+                                   queue=self.callback_queue)
+
+    def on_response(self, ch, method, props, body):
+        if self.corr_id == props.correlation_id:
+            self.response = body
+
+    def call(self, n):
+        tojson = json.dumps(n)
+        tosend = pickle.dumps(tojson)
+        self.response = None
+        self.corr_id = str(uuid.uuid4())
+        self.channel.basic_publish(exchange='',
+                                   routing_key='rpc_queue',
+                                   properties=pika.BasicProperties(
+                                         reply_to = self.callback_queue,
+                                         correlation_id = self.corr_id,
+                                         ),
+                                   body=tosend)
+        while self.response is None:
+            self.connection.process_data_events()
+        return pickle.loads(self.response)
+
+myclient = Client()
+
+response = myclient.call({"type" : "register"})
+print(" [.] Got %r" % response)
