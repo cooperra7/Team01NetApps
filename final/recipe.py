ed7021d5bda468bd66ca9b8d3e9036f9a3cede69
Zachary Yee <zjwy99@vt.edu>
2017-05-08 23:13:10 -0400

    Fix tabs

--- final/recipe.py
+++ final/recipe.py
@@ -9,70 +9,70 @@ import requests
 
 class Recipe(object):
 
-    def recipe(self, ch, method, properties, body):
-        print('Recipe')
-	temp = json.loads(pickle.loads(body))
-	id = temp['id']
-	ingredients = ''
-	if (id == 1010)
-		ingredients = "apple"
-	if (id == 1009)
-        	ingredients = "orange"
-	if (id == 1012)
-		ingredients = "carrot"
-        number = '1'
-	self.db.inventory.update({"id":id}, {"$inc":{"number":-1}})
+	def recipe(self, ch, method, properties, body):
+		print('Recipe')
+		temp = json.loads(pickle.loads(body))
+		id = temp['id']
+		ingredients = ''
+		if (id == 1010)
+			ingredients = "apple"
+		if (id == 1009)
+			ingredients = "orange"
+		if (id == 1012)
+			ingredients = "carrot"
+		number = '1'
+		self.db.inventory.update({"id":id}, {"$inc":{"number":-1}})
 
-        response = requests.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?",
-            headers={
-                "X-Mashape-Key": "1nMwLi8qDomshJNqUsfRJSDTcZHCp1ZopPPjsnaY7yCIdfAnVX",
-                "Accept": "application/json"
-            },
-            params={
-                'ingredients': ingredients,
-                'number': number,
-                'fillIngredients': 'true',
-                'limitLicense': 'true',
-                'ranking': '1'
-            }
-            )
-        ch.basic_publish(exchange='', routing_key=properties.reply_to,
-                         properties=pika.BasicProperties(correlation_id=properties.correlation_id),
-                         body=pickle.dumps(response.json()))
+		response = requests.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?",
+		    headers={
+			"X-Mashape-Key": "1nMwLi8qDomshJNqUsfRJSDTcZHCp1ZopPPjsnaY7yCIdfAnVX",
+			"Accept": "application/json"
+		    },
+		    params={
+			'ingredients': ingredients,
+			'number': number,
+			'fillIngredients': 'true',
+			'limitLicense': 'true',
+			'ranking': '1'
+		    }
+		    )
+		ch.basic_publish(exchange='', routing_key=properties.reply_to,
+				 properties=pika.BasicProperties(correlation_id=properties.correlation_id),
+				 body=pickle.dumps(response.json()))
 
-    def callback (self, ch, method, properties, body):
-        mess = pickle.loads (body)
-        obj = json.loads(mess)
-        if (obj['type'] == "recipe"):
-            self.recipe(ch, method, properties, body)
-        else:
-            print ('Not Valid')
+	def callback (self, ch, method, properties, body):
+		mess = pickle.loads (body)
+		obj = json.loads(mess)
+		if (obj['type'] == "recipe"):
+		    self.recipe(ch, method, properties, body)
+		else:
+		    print ('Not Valid')
 
-    def __init__(self):
-        self.client = MongoClient('localhost', 27017)
-        self.db = self.client.inventory_db
-	self.db = MongoClient().inventory_db
-	apples = {"item":"Apple", "id":1010, "number":5, "price":1}
-	oranges = {"item":"Orange", "id":1009, "number":100, "price":100}
-	carrots = {"item":"Carrot", "id":1012, "number":1000000, "price":1000000}
+	def __init__(self):
+		self.client = MongoClient('localhost', 27017)
+		self.db = self.client.inventory_db
+		self.db = MongoClient().inventory_db
+		apples = {"item":"Apple", "id":1010, "number":5, "price":1}
+		oranges = {"item":"Orange", "id":1009, "number":100, "price":100}
+		carrots = {"item":"Carrot", "id":1012, "number":1000000, "price":1000000}
 
-	self.db.inventory.insert(apples)
-	self.db.inventory.insert(oranges)
-	self.db.inventory.insert(carrots)
+		self.db.inventory.insert(apples)
+		self.db.inventory.insert(oranges)
+		self.db.inventory.insert(carrots)
 
-        vhost = '/'
-        mbservip = sys.argv[1]
-        creds = pika.PlainCredentials('guest', 'guest')
-        params = pika.ConnectionParameters(virtual_host=vhost, credentials=creds, host=mbservip)
-        self.connection = pika.BlockingConnection(params)
-        self.channel = self.connection.channel()
-        #        self.channel.exchange_declare (exchange = 'smartgroceries', type='direct')
-        result = self.channel.queue_declare(queue='recipe', exclusive=True)
-        self.qname = result.method.queue
+		vhost = '/'
+		mbservip = sys.argv[1]
+		creds = pika.PlainCredentials('guest', 'guest')
+		params = pika.ConnectionParameters(virtual_host=vhost, credentials=creds, host=mbservip)
+		self.connection = pika.BlockingConnection(params)
+		self.channel = self.connection.channel()
+		#        self.channel.exchange_declare (exchange = 'smartgroceries', type='direct')
+		result = self.channel.queue_declare(queue='recipe', exclusive=True)
+		self.qname = result.method.queue
 
-        #        self.channel.queue_bind (exchange='smartgroceries', queue=qname, routing_keys='accounts')
+		#        self.channel.queue_bind (exchange='smartgroceries', queue=qname, routing_keys='accounts')
 
-        self.channel.basic_consume(self.callback, queue=self.qname, no_ack=True)
-        self.channel.start_consuming()
+		self.channel.basic_consume(self.callback, queue=self.qname, no_ack=True)
+		self.channel.start_consuming()
 
 q = Recipe()
